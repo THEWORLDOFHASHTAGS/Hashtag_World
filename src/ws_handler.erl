2120253c2fcd2c52aa7fd0634ac87a6147160e66
Viktor Lantz <viktor-lantz@hotmail.com>
2015-12-16 12:16:59 +0100

    Added function which removes duplicate data on website

--- src/ws_handler.erl
+++ src/ws_handler.erl
@@ -9,8 +9,8 @@
 }).
 
 init({tcp, http}, _Req, _Opts) ->
-    %% start couchbeam and dependencies
-    application:start(asn1),
+	%% start couchbeam and dependencies
+	application:start(asn1),
     application:start(crypto),
     application:start(public_key),
     application:start(ssl),
@@ -20,11 +20,15 @@ init({tcp, http}, _Req, _Opts) ->
     application:start(hackney),
     application:start(couchbeam),
     %% connect to couchdb
-    couchbeam:server_connection("127.0.0.1", 5984, "", []),
-    {upgrade, protocol, cowboy_websocket}.
+	%% Username = "group6",
+	%% Password = "Hashtag1",
+	%% Options = [{basic_auth, {Username, Password}}],
+    	%% couchbeam:server_connection("129.16.155.39", 22, "", Options),
+	couchbeam:server_connection("127.0.0.1", 5984, "", []),
+	{upgrade, protocol, cowboy_websocket}.
 
 websocket_init(_TransportName, Req, _Opts) ->
-    {ok, Req, #state{}}.
+	{ok, Req, #state{}}.
 
 %% handle a connection to the socket
 %% receive the country Id as 'Msg' and open the document in the database with that _id 
@@ -33,26 +37,32 @@ websocket_handle({text, Msg}, Req, State) ->
     {ok, Doc} = open_doc(Msg),
     % get the data stored in the 'tweet' field from the document
     Data = couchbeam_doc:get_value(<<"data">>, Doc),
-    io:format("Retrieved: ~p~n", [Data]),
+	NoDubsData = remove_dups(Data),
+	io:format("List : ~p~n", [NoDubsData]),
+    % count(Data),
     % send a reply to the website with the data
-    {reply, {text, Data}, Req, State};
+	{reply, {text, NoDubsData}, Req, State};
 
 websocket_handle(_Data, Req, State) ->
-    {ok, Req, State}.
+	{ok, Req, State}.
 
 websocket_info({timeout, _Ref, Msg}, Req, State) ->
-    {reply, {text, Msg}, Req, State};
+	{reply, {text, Msg}, Req, State};
 
 websocket_info(_Info, Req, State) ->
-    {ok, Req, State}.
+	{ok, Req, State}.
 
 websocket_terminate(_Reason, _Req, _State) ->
-    ok.
+	ok.
 
 %% connect to couchdb
 
 connect_db() ->
-    couchbeam:server_connection("127.0.0.1", 5984, "", []).
+    	%% Username = "group6",
+	%% Password = "Hashtag1",
+	%% Options = [{basic_auth, {Username, Password}}],
+    %%couchbeam:server_connection("129.16.155.39", 22, "", Options).
+	couchbeam:server_connection("127.0.0.1", 5984, "", []).
 
 %% open the database 'countries'
 connect_table() ->
@@ -62,3 +72,13 @@ connect_table() ->
 open_doc(Id) ->
     {ok, Db} = connect_table(),
     couchbeam:open_doc(Db, Id).
+
+%count(String, Data) -> count(String, Data, 0).
+
+
+%count(String, [String|Tail], Count) -> count(String, Tail, Count+1);
+%count(String, [_|Tail], Count) -> count(String, Tail, Count);
+%count(String, [], Count) -> io:format("Retrieved: ~p~n", [{String,Count}]).
+
+remove_dups([])    -> [];
+remove_dups([H|T]) -> [H | [X || X <- remove_dups(T), X /= H]].
